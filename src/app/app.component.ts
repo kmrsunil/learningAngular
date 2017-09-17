c6843274504552a92c9823b29722b4f7b54a026b
Sunil Kumar <sunil.kumar3@kelltontech.com>
2017-09-17 13:50:48 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,38 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  //template : '<p>Title</p>',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+/*------------------------------*/
+/*------------------------------*/
+    name1 = "Sunil";
+    day  = new Date('12 10 1990');
+    arr1 : number[] = [4,15,13,1,63,678,88];
+;/*------------------------------*/
+    items = ["angular 4","React","Underscore"];
+    newItem = "";
+    pushItem = function(){
+        if(this.newItem != ""){
+            this.items.push(this.newItem);
+            this.newItem = "";
+        }
+    }
+    removeItem = function(index){
+        this.items.splice(index,1);
+    }
+/*------------------------------*/
   title = 'app works!';
+  obj = {
+    id : "1",
+    name : "umair"
+    }
+    arr = ["abc","def","gh1"];
+    isTru = true;
+    myName = "Sunil";
 }
+
+
